5a2cfadeb87976f6ca84d5c024c0d5dda34b468c
Andreas Reinhard <a.reinhard@me.com>
2017-07-03 17:00:44 +0200

    Some CSS Fixes

--- themes/SimpleBootstrap/modules/faq/faq.js
+++ themes/SimpleBootstrap/modules/faq/faq.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
 	$('li.faqblock > a').addClass('btn').addClass('btn-sm').addClass('btn-primary');
+	$('.accordion-content').each(function(){
+		$(this).css('display','none');
+	});
 });
