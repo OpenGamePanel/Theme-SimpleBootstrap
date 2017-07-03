4c65b39303f7bb4c4da69cbe91be403fa54e9121
Andreas Reinhard <a.reinhard@me.com>
2017-07-03 12:15:12 +0200

    Changed LiteFM Style

--- themes/SimpleBootstrap/modules/litefm/litefm.js
+++ themes/SimpleBootstrap/modules/litefm/litefm.js
@@ -5,9 +5,5 @@ $(document).ready(function() {
 	$('img[src="images/folder.png"]').replaceWith('<i class="fa fa-folder-open-o" aria-hidden="true"></i>');
 	$('img[src="images/txt.png"]').replaceWith('<i class="fa fa-pencil-square-o" aria-hidden="true"></i>');
         $('img[src="images/exec.png"]').replaceWith('<i class="fa fa-cog" aria-hidden="true"></i>');
-        $('img[src=""]').replaceWith('');
-        $('img[src=""]').replaceWith('');
-        $('img[src=""]').replaceWith('');
-
 
 });
