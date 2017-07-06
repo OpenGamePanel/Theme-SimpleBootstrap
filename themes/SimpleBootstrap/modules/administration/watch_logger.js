0bda47321f972a3e082340a32ca937cd82c7ff14
Andreas Reinhard <a.reinhard@me.com>
2017-07-06 13:00:19 +0200

    Replaced Delete Button on Watch Logger

--- themes/SimpleBootstrap/modules/administration/watch_logger.js
+++ themes/SimpleBootstrap/modules/administration/watch_logger.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
 	$('table').addClass('table').addClass('table-sm').addClass('table-striped').removeAttr('style');
 	var inputs = $('input, textarea, select').not(':input[type=button], :input[type=submit], :input[type=reset], :input[type=radio], :input[type=checkbox], :input[type=image]');
 	$(inputs).addClass('form-control').removeAttr('style');
+
+	$('[name="remove_log"]').replaceWith('<button type="submit" class="btn btn-xs btn-danger" name="remove_log" onclick="this.form.submit();"><i class="fa fa-trash" aria-hidden="true"></i></button>');
 });
