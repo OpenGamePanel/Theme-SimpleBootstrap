31728c968791c9f92fd6637ff806653935655056
oNdsen <a.reinhard@me.com>
2017-07-07 22:59:52 +0200

    Global Bootstrap File Upload Hook

--- themes/SimpleBootstrap/js/main.js
+++ themes/SimpleBootstrap/js/main.js
@@ -313,9 +313,27 @@ $(document).ready(function() {
 
 
 
-        $(window).load(function () {
-		$("body > .container-fluid").fadeIn(50);
-        });
+});
 
+$(window).load(function(){
+	$("body > .container-fluid").fadeIn(50);
+
+        $(':file').on('fileselect', function(event, numFiles, label) {
+                var input = $(this).parents('.input-group').find(':text'),
+                log = numFiles > 1 ? numFiles + ' files selected' : label;
+                if(input.length){
+                        input.val(log);
+                }else{
+                        if(log) alert(log);
+                }
+        });
+});
+
+
+$(document).on('change', ':file', function() {
+        var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
 });
 
--- themes/SimpleBootstrap/modules/settings/themes.js
+++ themes/SimpleBootstrap/modules/settings/themes.js
@@ -90,25 +90,6 @@ $(window).load(function(){
                         location.reload();
                 }
         });
-
-
-	$(':file').on('fileselect', function(event, numFiles, label) {
-		var input = $(this).parents('.input-group').find(':text'),
-		log = numFiles > 1 ? numFiles + ' files selected' : label;
-		if(input.length){
-			input.val(log);
-		}else{
-			if(log) alert(log);
-		}
-	});
-});
-
-
-$(document).on('change', ':file', function() {
-	var input = $(this),
-	numFiles = input.get(0).files ? input.get(0).files.length : 1,
-	label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-	input.trigger('fileselect', [numFiles, label]);
 });
 
 function get_active_loader() {
