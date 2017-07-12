0227f6ddbd3748dca88af7c1fed3ec02091cf289
Andreas Reinhard <a.reinhard@me.com>
2017-07-12 14:00:38 +0200

    Cron Module Updates

--- themes/SimpleBootstrap/modules/cron/cron.js
+++ themes/SimpleBootstrap/modules/cron/cron.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-	$('.main [href="?m=administration&p=main"]').addClass('btn').addClass('btn-sm').addClass('btn-primary');
+	$('.main [href="?m=administration&p=main"], [href="javascript:history.go(-1)"]').addClass('btn').addClass('btn-sm').addClass('btn-primary');
 	$('.main > .log').wrap('<div class="table-responsive"/>');
 	$('.main > table').wrap('<div class="table-responsive"/>');
+
+	$('.hundred td[colspan="6"]').attr('colspan',8);
+	$('.hundred th:last-of-type').attr('colspan',2);
 });
--- themes/SimpleBootstrap/modules/cron/user_cron.js
+++ themes/SimpleBootstrap/modules/cron/user_cron.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.main [href="?m=administration&p=main"]').addClass('btn').addClass('btn-sm').addClass('btn-primary');
+	$('.main [href="?m=administration&p=main"], [href="javascript:history.go(-1)"], .main [href="home.php?m=cron&p=cron"]').addClass('btn').addClass('btn-sm').addClass('btn-primary');
 	$('input[name="removeJob"]').removeClass('btn-primary').addClass('btn-danger');
 
 	$('.main > .log, .main > form, .main > table').wrap('<div class="table-responsive"/>');
