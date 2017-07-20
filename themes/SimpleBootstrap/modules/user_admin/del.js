6482256007cc90eb67c4d0cc31d907126dae5a37
Andreas Reinhard <a.reinhard@me.com>
2017-07-20 10:52:52 +0200

    Added User Admin Styles

---
+++ themes/SimpleBootstrap/modules/user_admin/del.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+        $('.main p:first-of-type').addClass('alert').addClass('alert-danger');
+	$('.main [href$="&y=y"]').addClass('btn').addClass('btn-sm').addClass('btn-danger');
+	$('.main [href="?m=user_admin"]').addClass('btn').addClass('btn-sm').addClass('btn-primary');
+});
+
