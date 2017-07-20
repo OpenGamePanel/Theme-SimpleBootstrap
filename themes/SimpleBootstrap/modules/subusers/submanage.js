79f431cff0038b4223706ed4b026fccb353a616a
Andreas Reinhard <a.reinhard@me.com>
2017-07-20 10:53:07 +0200

    Added Subuser Styles

---
+++ themes/SimpleBootstrap/modules/subusers/submanage.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+        $('.main [href="?m=subusers&p=add"]').addClass('btn').addClass('btn-sm').addClass('btn-primary');
+        $('.main [href="?m=subusers&p=del"]').addClass('btn').addClass('btn-sm').addClass('btn-danger');
+});
+
