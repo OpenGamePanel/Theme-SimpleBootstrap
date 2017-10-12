66dfac0bb2dbf3333c29517db2599eac0932b820
oNdsen <a.reinhard@me.com>
2017-10-12 19:44:13 +0200

    Lower Case Module Name Fix

---
+++ themes/SimpleBootstrap/modules/tickets/submitticket.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	var counter = 0;
+	 $('form > .ticket_elementDiv').each(function(){
+		$(this).removeClass('ticket_elementDiv').addClass('input-group');
+
+		$(this).find('label').replaceWith('<span class="input-group-addon" id="addon'+counter+'">'+$(this).find('label').text()+'</span>');
+		$(this).find('input').attr('aria-describedby','addon'+counter);
+
+		counter = counter+1;
+	});
+});
