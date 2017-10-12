5e90991f8a5cb45a28afe6e10ec566ad211aa8e5
oNdsen <a.reinhard@me.com>
2017-10-12 19:44:00 +0200

    Lower Case Module Name Fix

--- themes/SimpleBootstrap/modules/Tickets/Tickets.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$('[href="?m=Tickets&p=submitticket"]').addClass('btn').addClass('btn-primary').addClass('btn-sm');
-	$('.no_tickets').removeClass('no_tickets').addClass('alert').addClass('alert-danger');
-
-});
-
