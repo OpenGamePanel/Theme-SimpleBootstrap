91d382d5a4f25ac6419b28d8b3c90ae1e2b4dc9f
oNdsen <a.reinhard@me.com>
2017-11-05 21:47:43 +0100

    Added EditConfigFiles Support

---
+++ themes/SimpleBootstrap/modules/editconfigfiles/editconfigfiles.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	$('.main [href^="?m=gamemanager&p=game_monitor"], .main [href^="?m=editconfigfiles"]').addClass('btn').addClass('btn-primary').addClass('btn-xs');
+});
---
+++ themes/SimpleBootstrap/modules/editconfigfiles/modify.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$('.main [href^="?m=editconfigfiles"]').addClass('btn').addClass('btn-primary').addClass('btn-xs');
+});
+
