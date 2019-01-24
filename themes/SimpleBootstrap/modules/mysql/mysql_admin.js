e51953996395bf6c51841890d396f6fbdbc1ca9e
oNdsen <a.reinhard@me.com>
2019-01-24 15:01:54 +0100

    Several Improvements

--- themes/SimpleBootstrap/modules/mysql/mysql_admin.js
+++ themes/SimpleBootstrap/modules/mysql/mysql_admin.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-        $('#servermonitor [href$="&delete"]').addClass('btn').addClass('btn-xs').addClass('btn-danger');
-        $('#servermonitor [href$="&edit"]').addClass('btn').addClass('btn-xs').addClass('btn-primary');
-	$('#servermonitor [href$="&assign"]').addClass('btn').addClass('btn-xs').addClass('btn-primary');
+        $('#servermonitor [href$="&delete"], #servermonitor [href$="&remove_db').addClass('btn').addClass('btn-xs').addClass('btn-danger');
+        $('#servermonitor [href$="&edit"], #servermonitor [href$="&assign"], #servermonitor [href$="&edit_db_settings"]').addClass('btn').addClass('btn-xs').addClass('btn-primary');
 
         $('#servermonitor .success').addClass('label').addClass('label-success').removeClass('success');
         $('#servermonitor .failure').addClass('label').addClass('label-danger').removeClass('failure');
